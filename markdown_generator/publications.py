84ac8b442d388ca41fb6d43bcfcc2ec8cfd422c1
Eamonn-Beatts <beatts.eamonn@gmail.com>
2026-06-23 15:42:04 +1000

    Wiped hardcoded template links

--- markdown_generator/publications.py
+++ markdown_generator/publications.py
@@ -1,6 +1,6 @@
 # Publications markdown generator for AcademicPages
 # 
-# Takes a TSV / CSV of publications with metadata and converts them for use with [academicpages.github.io](academicpages.github.io). 
+# Takes a TSV / CSV of publications with metadata and converts them for use with [eamonn-beatts.github.io](eamonn-beatts.github.io). 
 # Can be called via the command prompt by using `python3 publications.py [filename]`.
 
 # Data format
